1b4508b64d490adee5377e0e501eca3d7b52b02b
benny-ojeda <bojeda.it@gmail.com>
2020-05-05 22:25:14 -0500

    Fixed some bugs

--- test.py
+++
@@ -1,8 +0,0 @@
-a = 'cd'
-if len(a.split()) == 2 and a.split(' ', 1)[0] == 'cd':
-        # second_word=a.split(' ', 1)[1]
-        # print(second_word)
-        print(len(a))
-else:
-    #len(a.split()) == 2 and a.split(' ', 1)[0] == 'cd':
-    print("missing arg")
