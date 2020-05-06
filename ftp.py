87326863669bd4f504a0d8b22da13ae2e5671649
benny-ojeda <bojeda.it@gmail.com>
2020-05-05 22:19:56 -0500

    Fixed some bugs

--- ftp.py
+++ ftp.py
@@ -35,38 +35,67 @@ mkdir       pwd             rmdir           send        size
 """)
             if cmd == command[1]:
                 ftp.dir()
+
             if cmd == command[2]:
                 print(ftp.pwd())
-            if cmd.split(' ', 1)[0] == command[3]:
+
+            if cmd.split(' ', 1)[0] == command[3] and len(cmd.split()) == 2:
                 dirname = cmd.split(' ', 1)[1]
                 print(ftp.cwd(dirname))
-            if cmd.split(' ', 1)[0] == command[4]:
+            elif cmd.split(' ', 1)[0] == command[3] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
+            if cmd.split(' ', 1)[0] == command[4] and len(cmd.split()) == 2:
                 filename = cmd.split(' ', 1)[1]
                 print(ftp.retrbinary("RETR " + filename, open(str(filename), 'wb').write))
-            if cmd.split(' ', 1)[0] == command[5]:
+            elif cmd.split(' ', 1)[0] == command[4] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
+            if cmd.split(' ', 1)[0] == command[5] and len(cmd.split()) == 2:
                 filename = cmd.split(' ', 1)[1]
                 print(ftp.storbinary("STOR " + filename, open(filename, 'rb'), callback=None))
-            if cmd.split(' ', 1)[0] == command[6]:
+            elif cmd.split(' ', 1)[0] == command[5] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
+            if cmd.split(' ', 1)[0] == command[6] and len(cmd.split()) == 2:
                 dirname = cmd.split(' ', 1)[1]
                 print(ftp.mkd(dirname))
-            if cmd.split(' ', 1)[0] == command[7]:
+            elif cmd.split(' ', 1)[0] == command[6] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
+            if cmd.split(' ', 1)[0] == command[7] and len(cmd.split()) == 2:
                 dirname = cmd.split(' ', 1)[1]
                 print(ftp.rmd(dirname))
-            if cmd.split(' ', 1)[0] == command[8]:
+            elif cmd.split(' ', 1)[0] == command[7] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
+            if cmd.split(' ', 1)[0] == command[8] and len(cmd.split()) == 2:
                 filename = cmd.split(' ', 1)[1]
                 print(ftp.delete(filename))
-            if cmd.split(' ', 1)[0] == command[9]:
+            elif cmd.split(' ', 1)[0] == command[8] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
+            if cmd.split(' ', 1)[0] == command[9] and len(cmd.split()) == 2:
                 filename = cmd.split(' ', 1)[1]
                 print(ftp.size(filename))
-            if cmd.split(' ', 1)[0] == command[10]:
+            elif cmd.split(' ', 1)[0] == command[9] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
+            if cmd.split(' ', 1)[0] == command[10] and len(cmd.split()) == 2:
                 level = cmd.split(' ', 1)[1]
                 print(ftp.set_debuglevel(int(level)))
+            elif cmd.split(' ', 1)[0] == command[10] and len(cmd.split()) == 1:
+                print("E: Missing argument.")
+
             if cmd == command[11]:
                 os.system("clear")
+
             if cmd in ('q', 'quit', 'exit'):
                 return True
-            if cmd.split(' ', 1)[0] not in command:
+
+            if len(cmd.split()) and cmd.split(' ', 1)[0] not in command:
                 print(reminder)
+                
         except ftplib.all_errors as error:
                 print('FTP error: ', error)
 
